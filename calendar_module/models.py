9a3e67aebbbfbba51027e433a11505958d7c9ae3
BrodaJakub <jakub.broda@outlook.com>
2025-01-10 19:19:04 +0100

    Added Event module

--- calendar_module/models.py
+++ calendar_module/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Tytuł")
+    description = models.TextField(blank=True, verbose_name="Opis")
+    start_time = models.DateTimeField(verbose_name="Czas rozpoczęcia")
+    end_time = models.DateTimeField(verbose_name="Czas zakończenia")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="events", verbose_name="Użytkownik"
+    )
+
+    def __str__(self):
+        return self.title
