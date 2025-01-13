0d032e63061a78791e8528fb208993433b182c65
BrodaJakub <jakub.broda@outlook.com>
2025-01-13 21:22:33 +0100

    Added event detail page access restrictions

--- calendar_module/views.py
+++ calendar_module/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required # type: ignore
+from django.shortcuts import render, redirect, get_object_or_404 # type: ignore
+from django.http import HttpResponseForbidden # type: ignore
 from .forms import EventForm
 from datetime import date
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # type: ignore
 from .models import Event
 
 
@@ -35,8 +37,12 @@ def add_event(request):
     
     return render(request, 'add_event.html', {'form': form})
 
+@login_required
 def event_detail(request, event_id):
     # Pobranie wydarzenia lub zwrócenie błędu 404
     event = get_object_or_404(Event, id=event_id)
+
+    if event.user != request.user:
+        return HttpResponseForbidden("Nie masz dostępu do tego wydarzenia.")
     
     return render(request, 'event_detail.html', {'event': event})
